9743793b11372eb1b66c226ef69b54266249f96f
Willow <isaiasnoel@gmail.com>
2025-11-24 00:01:21 -0800

    typos

--- src/data/SolutionCards.ts
+++ src/data/SolutionCards.ts
@@ -14,7 +14,7 @@ export const solutionsInfos: SolutionInfo[] = [
   {
     cardTitle: "MARKET INTELLIGENCE",
     cardText:
-      "By deciphering competitor strategies, strengths, and potential vulnerabilities, through comprehensive competitive intelligence research from our team, we are able to provide our clients with a detailed roadmap for their own market positioning and growth.Alongside this, our team uses data models to project sales and cost trends to ensure that our clients can make informed decisions that are underpinned by rich market insights.",
+      "By deciphering competitor strategies, strengths, and potential vulnerabilities, through comprehensive competitive intelligence research from our team, we are able to provide our clients with a detailed roadmap for their own market positioning and growth. Alongside this, our team uses data models to project sales and cost trends to ensure that our clients can make informed decisions that are underpinned by rich market insights.",
     imageSrc: Trend,
   },
   {
