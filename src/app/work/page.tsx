6aeb478f33cb01f14b9650b65930fe0483fca9f0
Willow <isaiasnoel@gmail.com>
2025-12-13 12:36:07 -0800

    fragment tags added to all page.tsx

--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -11,24 +11,26 @@ import Carousel from "@/components/LogoCarousel";
 
 const Work = () => {
   return (
-    <div>
-      <Header title="Client Work" headerPicture={workHeader} />
-      <InfoSection
-        title="Our Services"
-        text="Established in Fall 2022 as the first and only consulting club on campus, Highlander Consulting Group is driven by a mission to offer members real-world experience through partnerships with diverse firms. We're dedicated to creating and delivering top-tier solutions that drive measurable results."
-        image={OurServicesImage}
-        imageAlt="Our Services"
-      />
-      <Carousel data={clientCarouselData} />
-      <Solutions />
-      <ContactUs
-        buttonText="Contact Us"
-        buttonHref="/"
-        text="Driven by a passion for consulting, a dedication to transformative
-          collaborations, and experience from top firms, we're always eager to
-          tackle fresh challenges."
-      />
-    </div>
+    <>
+      <div>
+        <Header title="Client Work" headerPicture={workHeader} />
+        <InfoSection
+          title="Our Services"
+          text="Established in Fall 2022 as the first and only consulting club on campus, Highlander Consulting Group is driven by a mission to offer members real-world experience through partnerships with diverse firms. We're dedicated to creating and delivering top-tier solutions that drive measurable results."
+          image={OurServicesImage}
+          imageAlt="Our Services"
+        />
+        <Carousel data={clientCarouselData} />
+        <Solutions />
+        <ContactUs
+          buttonText="Contact Us"
+          buttonHref="/"
+          text="Driven by a passion for consulting, a dedication to transformative
+            collaborations, and experience from top firms, we're always eager to
+            tackle fresh challenges."
+        />
+      </div>
+    </>
   );
 };
 
